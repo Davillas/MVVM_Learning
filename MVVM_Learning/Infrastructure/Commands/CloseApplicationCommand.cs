d06d782f769c0746ca223d13d6b33412d9348551
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-06-25 17:07:44 +0400

    Separate Close Command

---
+++ MVVM_Learning/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,15 @@
+﻿using MVVM_Learning.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace MVVM_Learning.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
