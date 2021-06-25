bc040544322b3f673940f41ff81b370ee9ecc858
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-06-25 11:52:06 +0400

    Testing Viewmodel of Main Window

---
+++ MVVM_Learning/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MVVM_Learning.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_Learning.ViewModels
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        #region MainWindow Title
+
+        private string _Title = "Test Title";
+        /// <summary> Main Window Title </summary>
+        public string Title 
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
