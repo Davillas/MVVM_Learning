e89ffb556edf8e083d9d7de3afa92bdcace1c289
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-06-25 11:06:12 +0400

    Test console app

---
+++ MVVM_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVVM_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
