d97df208dde3b6257de1abbd4a56c29d046e1040
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-06-28 09:36:44 +0400

    Testing obtaining data from server

--- MVVM_Console/Program.cs
+++ MVVM_Console/Program.cs
@@ -28,7 +28,7 @@ namespace MVVM_Console
             {
                 var line =  data_reader.ReadLine();
                 if (string.IsNullOrWhiteSpace(line)) continue;
-                yield return line;
+                yield return line.Replace("Korea," , "Korea -");
             }
         }
 
@@ -38,6 +38,22 @@ namespace MVVM_Console
             .Skip(4)
             .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
+
+
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData() 
+        {
+            var lines = GetDataLines()
+                .Skip(1).Select(line => line.Split(','));
+
+            foreach(var row in lines)
+            {
+                var province = row[0].Trim();
+                var country_name = row[1].Trim(' ', '"');
+                var counts = row.Skip(4).Select(int.Parse).ToArray();
+
+                yield return (country_name, province, counts);
+            }
+        }
         static void Main(string[] args)
         {
 
@@ -45,10 +61,11 @@ namespace MVVM_Console
 
             var response = client.GetAsync(data_url).Result;
             var csv_str = response.Content.ReadAsStringAsync().Result;*/
-            var dates = GetDates();
+            /*  var dates = GetDates();
 
-            Console.WriteLine(string.Join("\r\n", dates));
-
+              Console.WriteLine(string.Join("\r\n", dates));*/ 
+            var armenia = GetData().First(v => v.Country.Equals("Armenia", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(string.Join("\r\n", GetDates().Zip(armenia.Counts, (date, count) => $"{date:dd:MM} - {count}")));
             Console.ReadLine();
         }
     }
