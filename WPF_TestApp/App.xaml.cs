7714f5f12b789a4ef0883e0c204b7c7b7ed29ba5
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-06-30 12:50:48 +0400

    Cleaning (cont. cont.)

--- WPF_TestApp/App.xaml.cs
+++ WPF_TestApp/App.xaml.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace WPF_TestApp
+﻿namespace WPF_TestApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
