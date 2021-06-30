483492d9d5d1b25b8dfa742e27361de364fd9059
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-06-30 12:47:59 +0400

    Cleaning (cont.)

--- WPF_TestApp/App.xaml.cs
+++ WPF_TestApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace WPF_TestApp
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
