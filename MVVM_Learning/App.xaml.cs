8cfb6d746254dfc1ac417cbfa9ddc33c33903744
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-06-30 12:46:01 +0400

    Cleaning

--- MVVM_Learning/App.xaml.cs
+++ MVVM_Learning/App.xaml.cs
@@ -5,7 +5,7 @@ namespace MVVM_Learning
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
