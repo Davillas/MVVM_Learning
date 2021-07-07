2459864a77b8eabc7340764621f8fa90851739cd
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-07-07 09:36:22 +0400

    Host Building infrastructure

--- MVVM_Learning/App.xaml.cs
+++ MVVM_Learning/App.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Linq;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MVVM_Learning.Models;
 using MVVM_Learning.Services;
+using MVVM_Learning.ViewModels;
 
 namespace MVVM_Learning
 {
@@ -10,11 +14,18 @@ namespace MVVM_Learning
     public partial class App : Application
     {
         public static bool IsDesignMode { get; private set; } = true;
+
+        
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+        }
 
-            
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<DataService>();
+            services.AddSingleton<CountriesStatisticsViewModel>();
         }
     }
 }
--- MVVM_Learning/Program.cs
+++ MVVM_Learning/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace MVVM_Learning
 {
@@ -13,6 +15,22 @@ namespace MVVM_Learning
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(args);
+
+            host_builder.UseContentRoot(Environment.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", true, true);
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+
+            return host_builder;
         }
     }
 }
