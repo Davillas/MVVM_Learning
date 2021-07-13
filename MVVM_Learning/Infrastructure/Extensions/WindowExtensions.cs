bfd9175f808f3e63cad64f33cd67b9c07aa556c9
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-07-13 14:18:23 +0400

    System menu for app icon

---
+++ MVVM_Learning/Infrastructure/Extensions/WindowExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+using MVVM_Learning.Infrastructure.Extensions;
+
+namespace System.Windows
+{
+    static class WindowExtensions
+    {
+        private const string user32 = "user32.dll";
+
+        [DllImport(user32, CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        public static IntPtr SendMessage(this Window window, WM Msg, SC wParam, IntPtr lParam = default)
+        {
+            return SendMessage(window.GetWindowHandle(), (uint) Msg, (IntPtr) wParam, lParam);
+        }
+
+        public static IntPtr GetWindowHandle(this Window window)
+        {
+            var helper = new WindowInteropHelper(window);
+            return helper.Handle;
+        }
+    }
+}
