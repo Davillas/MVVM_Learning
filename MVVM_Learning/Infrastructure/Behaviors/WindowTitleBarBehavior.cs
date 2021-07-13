b6c6cba84612cb43e3fce7e6105c1da42a104495
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-07-13 10:33:45 +0400

    Adding Extensions for DependencyObject to find parent element

---
+++ MVVM_Learning/Infrastructure/Behaviors/WindowTitleBarBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace MVVM_Learning.Infrastructure.Behaviors
+{
+
+    class WindowTitleBarBehavior : Behavior<UIElement>
+    {
+        private Window _Window;
+
+        protected override void OnAttached()
+        {
+            _Window = AssociatedObject as Window ?? AssociatedObject.Find;
+        }
+
+        protected override void OnDetaching()
+        {
+            
+        }
+    }
+}
