f7685c3f1040f375019a476a053c4e405f522267
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-07-13 09:48:34 +0400

    Extension methods for DependencyObject, allowing to find its root in VisualTree

---
+++ MVVM_Learning/Infrastructure/Behaviors/CloseWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+
+namespace MVVM_Learning.Infrastructure.Behaviors
+{
+    class CloseWindow : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.Click += OnButtonClick;
+        }
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Click -= OnButtonClick;
+        }
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var button = AssociatedObject;
+
+            var window = FindVisualRoot(button) as Window;
+            window?.Close();
+        }
+
+        private static DependencyObject FindVisualRoot(DependencyObject obj)
+        {
+            do
+            {
+                var parent = VisualTreeHelper.GetParent(obj);
+                if (parent is null) return obj;
+                obj = parent;
+
+            } while (true);
+        }
+    }
+}
---
+++ MVVM_Learning/Infrastructure/Behaviors/WindowStateChange.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace MVVM_Learning.Infrastructure.Behaviors
+{
+    class WindowStateChange : Behavior<Button>
+    {
+    }
+}
