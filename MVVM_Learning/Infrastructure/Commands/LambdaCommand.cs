9e16662482566e71db3a92f3f75afdc04be8fcb8
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-07-14 15:54:16 +0400

    Add/Edit Students Commands Templates

--- MVVM_Learning/Infrastructure/Commands/LambdaCommand.cs
+++ MVVM_Learning/Infrastructure/Commands/LambdaCommand.cs
@@ -17,6 +17,10 @@ namespace MVVM_Learning.Infrastructure.Commands
 
         public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
 
-        public override void Execute(object parameter) => _Execute(parameter);
+        public override void Execute(object parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            _Execute(parameter);
+        }
     }
 }
--- MVVM_Learning/ViewModels/StudentManagementViewModel.cs
+++ MVVM_Learning/ViewModels/StudentManagementViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using MVVM_Learning.Infrastructure.Commands;
 using MVVM_Learning.Models.DeanOffice;
 using MVVM_Learning.Services.Students;
 using MVVM_Learning.ViewModels.Base;
@@ -52,6 +54,52 @@ namespace MVVM_Learning.ViewModels
             get => _SelectedStudent;
             set => Set(ref _SelectedStudent, value);
         }
+
+        #endregion
+
+        #region Commands
+
+        #region Command AddStudentCommand - Adding Student Command
+
+        /// <summary>Adding Student Command</summary>
+        private ICommand _AddStudentCommand;
+
+        /// <summary>Adding Student Command</summary>
+        public ICommand AddStudentCommand => _AddStudentCommand
+            ??= new LambdaCommand(OnAddStudentCommandExecuted, CanAddStudentCommandExecute);
+
+        /// <summary>Проверка возможности выполнения - Adding Student Command</summary>
+        private bool CanAddStudentCommandExecute(object p) => p is Group;
+
+        /// <summary>Логика выполнения - Adding Student Command</summary>
+        private void OnAddStudentCommandExecuted(object p)
+        {
+            var group = (Group) p;
+        }
+
+        #endregion
+
+        #region Command EditStudentCommand - Edit Student
+
+        /// <summary>Edit Student</summary>
+        private ICommand _EditStudentCommand;
+
+        /// <summary>Edit Student</summary>
+        public ICommand EditStudentCommand => _EditStudentCommand
+            ??= new LambdaCommand(OnEditStudentCommandExecuted, CanEditStudentCommandExecute);
+
+        /// <summary>Проверка возможности выполнения - Edit Student</summary>
+        private bool CanEditStudentCommandExecute(object p) => p is Student;
+
+        /// <summary>Логика выполнения - Edit Student</summary>
+        private void OnEditStudentCommandExecuted(object p)
+        {
+            var student = (Student) p;
+
+
+        }
+
+        #endregion
 
         #endregion
 
